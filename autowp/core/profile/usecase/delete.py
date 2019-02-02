9d919244c74b9af25a899306da336f27950dd891
hiraqdev <hiraq.dev@gmail.com>
2019-02-02 16:08:21 +0700

    Fix profile usecase: delete & unit tests

--- autowp/core/profile/usecase/delete.py
+++ autowp/core/profile/usecase/delete.py
@@ -1,4 +1,29 @@
+from typing import NoReturn
+
+from autowp.core.shared.utils import typechecker
 from autowp.core.shared.base import UseCase 
+from autowp.core.profile.repository import ProfileRepository
 
 class DeleteUseCase(UseCase):
-	pass
+	"""A usecase used when removing profile from database
+
+	Attributes:
+		repo: Should be an instance of ProfileRepository
+	"""
+	def __init__(self, repo: ProfileRepository) -> NoReturn:
+		"""Initialize object instance
+		
+		Raises:
+			core.shared.exceptions.VarTypeError: If repo is not an instance of ProfileRepository
+		"""
+		typechecker.check(repo, ProfileRepository, ('repo', 'ProfileRepository'))
+		self.repo = repo
+
+	def remove(self, name: str) -> bool:
+		"""Delete profile from database
+		
+		Raises:
+			core.shared.exceptions.VarTypeError: If given name is not a string
+			core.shared.exceptions.StorageError: If there is an error relate with storage
+		"""
+		return self.repo.remove(name)
---
+++ autowp/tests/test_core/test_profile/test_usecase/test_delete.py
@@ -0,0 +1,65 @@
+import unittest
+from typing import Optional, List
+
+from autowp.core.shared.utils import typechecker
+from autowp.core.shared.exceptions import VarTypeError
+
+from autowp.core.profile.entity import Profile
+from autowp.core.profile.repository import ProfileRepository, Options
+from autowp.core.profile.usecase.register import RegisterUseCase
+from autowp.core.profile.usecase.delete import DeleteUseCase 
+
+class MemoryProfileRepo(ProfileRepository):
+
+	def __init__(self):
+		self.in_memory = {}
+
+	def create(self, profile: Profile) -> bool:
+		self.in_memory[profile.name] = profile
+		return True
+
+	def get_list(self, options: Options) -> List[Profile]:
+		pass 
+
+	def get_detail(self, name: str) -> Optional[Profile]:
+		if name in self.in_memory:
+			return self.in_memory.get(name)
+
+		return None
+
+	def remove(self, name: str) -> bool:
+		typechecker.check(name, str, ('name', 'str'))
+
+		check = self.get_detail(name)
+		if check:
+			del self.in_memory[name]
+			return True
+
+		return False 
+
+class DeleteProfileTestCase(unittest.TestCase):
+
+	def test_delete_success(self):
+		repo = MemoryProfileRepo()
+		profile = Profile('myname', 'mypassword')
+		
+		register_case = RegisterUseCase(repo)
+		delete_case = DeleteUseCase(repo)
+
+		self.assertTrue(register_case.register(profile))
+		self.assertTrue(delete_case.remove(profile.name))
+
+		check = repo.get_detail(profile.name)
+		self.assertIsNone(check)
+
+	def test_delete_mismatch_type(self):
+		with self.assertRaises(VarTypeError):
+			repo = MemoryProfileRepo()
+			delete_case = DeleteUseCase(repo)
+			delete_case.remove(12)
+
+	def test_delete_not_exist(self):
+		repo = MemoryProfileRepo()
+		delete_case = DeleteUseCase(repo)
+
+		self.assertFalse(delete_case.remove('not existing name'))
