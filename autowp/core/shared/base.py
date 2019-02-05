eb016685f9e285966fb090eb67eff051b50482ca
hiraqdev <hiraq.dev@gmail.com>
2019-02-06 00:22:13 +0700

    Adding shared entity for state

--- autowp/core/shared/base.py
+++ autowp/core/shared/base.py
@@ -1,3 +1,4 @@
+from typing import Optional, Dict, Any
 from abc import ABCMeta, abstractmethod
 
 class Repository(metaclass=ABCMeta):
@@ -22,4 +23,27 @@ class PasswordHasher(metaclass=ABCMeta):
 		"""This method should be implemented for any 
 		hasher subclasses, to implement their hash algorithm
 		"""
+		pass
+
+class Tokenizer(metaclass=ABCMeta):
+	"""This class should be used when building session
+	token
+
+	Attributes:
+		salt: A string act as salt		
+		payload: A dictionary data that need to encode
+		options: A dictionary used for options
+	"""
+
+	def __init__(self, salt: str, payload: dict, options: Optional[Dict[str, Any]] = None):
+		self.salt = salt
+		self.payload = payload
+		self.options = options
+
+	@abstractmethod
+	def encode(self) -> str:
+		pass
+
+	@abstractmethod
+	def decode(self, token: str) -> Optional[Dict[str, Any]]:
 		pass
